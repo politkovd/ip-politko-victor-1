511dbefacefeb4b0a24853712e78f3c9dacb980a
politkovd <russia-163@yandex.ru>
2020-08-10 20:51:32 +0400

    First commit

---
+++ TESTPVD.py
@@ -0,0 +1,13 @@
+a = 1.3
+b = 36
+c = a + b
+print(c)
+
+
+#x = input ('enter youre age: ')
+#print (int(x))
+
+age = int(input('Сколько тебе лет?'))git
+name = input("Введи свое имя")
+print (age, name)
+
