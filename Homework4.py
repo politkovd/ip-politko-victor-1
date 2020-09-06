e324013d4d415ee40100b006d57f64873798f601
politkovd <russia-163@yandex.ru>
2020-09-06 12:37:12 +0400

    HW 3/4

--- Homework4.py
+++ Homework4.py
@@ -3,24 +3,25 @@
 # т.е функция ничего не возвращает, а выводит на консоль ответ самостоятельно
 # Предполагается, что 1км = 1,609 мили
 
-# def conv(km=1, mil=1.609):
-#      return km * mil
-# print(conv())
+def conv(km):
+    mil = 1.609
+    return km * mil
+print(conv(5))
 
 
 
-# Задание-2:
-# Напишите функцию, округляющую полученное произвольное десятичное число
-# до кол-ва знаков (кол-во знаков передается вторым аргументом).
-# Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
-# Для решения задачи не используйте встроенные функции и функции из модуля math.
-# def my_round(number, ndigits):
-#             number=number*(10**ndigits)+0.4
-#             number=number//1
-#             return number/(10**ndigits)
-# print(my_round(2.1234567, 5))
-# print(my_round(2.1999967, 5))
-# print(my_round(2.9999967, 5))
+# # Задание-2:
+# # Напишите функцию, округляющую полученное произвольное десятичное число
+# # до кол-ва знаков (кол-во знаков передается вторым аргументом).
+# # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
+# # Для решения задачи не используйте встроенные функции и функции из модуля math.
+def my_round(number, ndigits):
+            number=number*(10**ndigits)+0.4
+            number=number//1
+            return number/(10**ndigits)
+print(my_round(2.1234567, 5))
+print(my_round(2.1999967, 5))
+print(my_round(2.9999967, 5))
 
 
 
@@ -33,19 +34,19 @@
 
 
 
-# def lucky_ticket(ticket):
-#     num = str(ticket)
-#     lst1 = int(num[0]) + int(num[1]) + int(num[2])
-#     lst2 = int(num[-1]) + int(num[-2]) + int(num[-3])
-#     if lst1 == lst2:
-#         return True
-#     else:
-#         return False
+def lucky_ticket(ticket):
+    num = str(ticket)
+    lst1 = int(num[0]) + int(num[1]) + int(num[2])
+    lst2 = int(num[-1]) + int(num[-2]) + int(num[-3])
+    if lst1 == lst2:
+        return True
+    else:
+        return False
 
-# print(lucky_ticket(123006))
-# print(lucky_ticket(12321))
-# print(lucky_ticket(436751))
-# print(lucky_ticket(126589))
+print(lucky_ticket(123006))
+print(lucky_ticket(12321))
+print(lucky_ticket(436751))
+print(lucky_ticket(126589))
 
 
 # NORMAL
@@ -54,57 +55,57 @@
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
-# def fibonacci(n, m):
-#     a, b = 1, 1
-#     f_list = [1, ]
+def fibonacci(n, m):
+    a, b = 1, 1
+    f_list = [1, ]
  
-#     for i in range(m):
-#         a, b = b, a + b
-#         f_list.append(a)
+    for i in range(m):
+        a, b = b, a + b
+        f_list.append(a)
  
-#     return f_list[n - 1:m]
+    return f_list[n - 1:m]
  
  
-# print('fibonacci(1, 6): ', fibonacci(1, 10))
+print('fibonacci(1, 6): ', fibonacci(1, 10))
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
 # Для сортировки используйте любой алгоритм (например пузырьковый).
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
 
-# def sort_to_max(origin_list):
-#     if len(origin_list) > 1:
-#         pivot_index = len(origin_list) // 2
-#         smaller_items = []
-#         larger_items = []
+def sort_to_max(origin_list):
+    if len(origin_list) > 1:
+        pivot_index = len(origin_list) // 2
+        smaller_items = []
+        larger_items = []
  
-#         for i, val in enumerate(origin_list):
-#             if i != pivot_index:
-#                 if val < origin_list[pivot_index]:
-#                     smaller_items.append(val)
-#                 else:
-#                     larger_items.append(val)
+        for i, val in enumerate(origin_list):
+            if i != pivot_index:
+                if val < origin_list[pivot_index]:
+                    smaller_items.append(val)
+                else:
+                    larger_items.append(val)
  
-#         sort_to_max(smaller_items)
-#         sort_to_max(larger_items)
-#         origin_list[:] = smaller_items + [origin_list[pivot_index]] + larger_items
+        sort_to_max(smaller_items)
+        sort_to_max(larger_items)
+        origin_list[:] = smaller_items + [origin_list[pivot_index]] + larger_items
  
-#     return origin_list
+    return origin_list
  
  
-# print(sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
+print(sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
 
 
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
-#  def filter_func(function, iterable):
-#      return (item for item in iterable if function(item))
+def filter_func(function, iterable):
+     return (item for item in iterable if function(item))
  
  
-#  print(list(filter_func(lambda x: True if x % 2 == 0 else False,
-#                    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])))
+print(list(filter_func(lambda x: True if x % 4 != 1 else False,
+                  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])))
 
 
 
@@ -113,13 +114,13 @@
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
 
-# def is_parallelogram(a1, a2, a3, a4):
-#     if abs(a3[0] - a2[0]) == abs(a4[0] - a1[0]) and \
-#        abs(a2[1] - a1[1]) == abs(a3[1] - a4[1]):
-#         return True
-#     return False
+def is_parallelogram(a1, a2, a3, a4):
+    if abs(a3[0] - a2[0]) == abs(a4[0] - a1[0]) and \
+       abs(a2[1] - a1[1]) == abs(a3[1] - a4[1]):
+        return True
+    return False
 
-# print(is_parallelogram((1,3),(1,5),(1,5),(1,3)))
+print(is_parallelogram((1,3),(1,5),(1,5),(1,3)))
 
 
 
@@ -136,24 +137,6 @@
 # Вывод: 1 17/42  (результат обязательно упростить и выделить целую часть)
 # Ввод: -2/3 - -2
 # Вывод: 1 1/3
-
-equation = '1 5/6 + 4/7'
-equation_list = equation.split()
-print(equation_list)
- 
-print(equation_list[0].isdigit())
- 
-if equation_list[0].isdigit():
-    print('это первая дробь с целой частью!')
-else:
-    print('это первая дробь без целой части!')
- 
-if equation_list[-2].isdigit():
-    print('это вторая дробь с целой частью!')
-else:
-    print('это вторая дробь без целой части!')
-
-
 
 
 
@@ -178,3 +161,11 @@ else:
 # Подсказка:
 # Чтобы получить список больших букв русского алфавита:
 # print(list(map(chr, range(ord('А'), ord('Я')+1))))
+
+from itertools import groupby
+with open("fruits.txt" ) as fail:
+	for a, b in groupby(fail.readlines(), key=lambda x: x[0]):
+		with open(a, "w") as out:
+			for x in b:
+				out.write(x)
+print(list(map(chr, range(ord('А'), ord('Я')+1))))
